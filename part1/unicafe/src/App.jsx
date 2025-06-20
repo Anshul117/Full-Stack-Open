331ee4f3d2c8f49322a0fc3897a55308d91fb489
Anshul117 <anshulverma1973@gmail.com>
2025-06-20 21:10:32 +0530

    dest

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,51 +5,41 @@ const Button = ({ onClick, text }) => {
   )
 
 }
-const Display = (props) => {
-  return (<div>
-    {props.text}   {props.att}
+const Statistics  = ({good ,neutral,bad}) =>{
+  const total = good + neutral + bad
+  const Avg =total==0 ? "No inputYet" : (good + bad*-1)/total
+  const positive = total ==0 ?"No inputYet" :(good)/total
+  return (
+    <div>
+      <h2> Statistics</h2>
+      <p>Good : {good}</p>
+      <p>Bad : {bad}</p>
+      <p> Neutral : {neutral}</p>
+      <p>Total : {total}</p>
+      <p> Avg : {Avg}</p>
+      <p> positive : {positive}</p>
 
-  </div>
+    </div>
   )
+    
+   
+      
+    
 }
+
 
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const total = good + bad + neutral
-  const avg = () =>{
-    if(total==0){
-      return " No Input Yet"
-    }
-    return ((good + bad*-1)/total).toFixed(2)
-  }
-  const TPos = () =>{
-    if(good==0){
-      return "No positive feedback"
-    }
-    console.log(bad)
-    return (good/total).toFixed(2)
-  }
-  
-  
-
   return (
     <div>
       <h1>Give FeedBack</h1>
       <Button onClick={()=> setGood(good+1)} text="good" />
      <Button onClick={() => setNeutral(neutral+1)} text="neutral" />
       <Button onClick={()=> setBad(bad+1)} text="bad" />
-
-
-      <h2>statistics</h2>
-      <Display text="good :" att={good} />
-      <Display text="neutral :" att={neutral} />
-      <Display text="bad :" att={bad} />
-      <Display text = "total" att = {good + neutral + bad} />
-      <Display text = " Average :" att = {avg()} />
-      <Display text = "Positive :" att = {TPos()} />
+      <Statistics good = {good} bad = {bad} neutral = {neutral} />
 
 
     </div>
