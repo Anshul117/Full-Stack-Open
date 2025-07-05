57af6546875b49fb4152dec32d5033d09d93ff7b
Anshul117 <anshulverma1973@gmail.com>
2025-07-06 00:57:59 +0530

    'phoneBook'

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,24 +1,37 @@
 import { useState } from 'react'
-
+const Display = (props) =>{
+  return(
+    <div><li>{props.att}</li></div>
+  )
+}
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
   ]) 
   const [newName, setNewName] = useState('')
+  const addName = (event) =>{
+    event.preventDefault()
+    const nameObj ={
+      name : newName
+    }
+    setPersons(persons.concat(nameObj))
+    setNewName('')
+
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form  onSubmit={addName}> 
         <div>
-          name: <input />
+          name:<input value={newName} onChange={e =>{setNewName(e.target.value)}}></input>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul> {persons.map((person,index) =>(<Display key ={index} att = { person.name} /> )  )}</ul>
     </div>
   )
 }
